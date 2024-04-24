b3e3c1f75fc9f7749e5404c916415359a8e79964
Bolzonleo <166229623+Bolzonleo@users.noreply.github.com>
2024-04-24 19:38:33 -0300

    CargaEjerciciosPseudocodigo

---
+++ EjerciciosPseudocodigo/EjerciciosPseudocodigo/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+//Ejercicio1:
+//Sumar 2 números
+//Console.Write("Ingrese un número: ");
+//int numero1, numero2;
+//if (!Int32.TryParse(Console.ReadLine(), out numero1))
+//{
+//    Console.WriteLine("Error, debe ingresar un número correcto");
+//}
+//else
+//{
+//    Console.Write("Ingrese otro número: ");
+//    if (!Int32.TryParse(Console.ReadLine(), out numero2))
+//    {
+//        Console.WriteLine("Error, debe ingresar un número correcto");
+//    }
+//    else
+//    {
+//        Console.WriteLine("La suma de los números es: " + (numero1 + numero2));
+//    }
+//}
+//Ejercicio2:
+//Determinar si un numero es par o impar
+//
+//Console.WriteLine("ingrese un numero:");
+//int number;
+//if (!Int32.TryParse(Console.ReadLine(), out number)) ;
+// {
+//    Console.Write("Error, numero invalido");
+// }
+//else
+// {
+//    if ((number % 2) == 0) ;
+//     {
+//        Console.WriteLine("Es par.");
+//     }
+//    else
+//     {
+//        Console.WriteLine("Es impar.");
+//     }
+//
+// }
+//Ejercicio3:
+//Determinar el area de un cuadrado
+//Console.WriteLine("ingresar la longitud de un lado del cuadrado: ");
+//float lado, area;
+//if (!float.TryParse(Console.ReadLine() out lado) || lado <= 0) ;
+//{
+//    Console.WriteLine("Error, numero no valido.");
+//}
+//else
+//{
+//    area = lado * lado;
+//    Console.WriteLine("El area del cuadrado es: " + area);
+//}
+//Ejercicio4:
+//Promedio de 3 numeros
+//Console.Write("Ingrese el primer numero: ");
+//float number1, number2, number3, prom;
+//if (!float.TryParse(Console.ReadLine(), out number1)) ;
+//{
+//    Console.WriteLine("Error, numero no valido");
+//}
+//else
+//{
+//    Console.Write("Ingrese el segundo numero: ");
+//    if (!float.TryParse(Console.ReadLine(), out number2)) ;
+//    {
+//        Console.WriteLine("Error, numero no valido");
+//    }
+//    else
+//    {
+//        Console.Write("Ingrese el tercer numero: ");
+//        if (!float.TryParse(Console.ReadLine(), out number3)) ;
+//        {
+//            Console.WriteLine("Error, numero no valido");
+//        }
+//        else
+//        {
+//            prom = (number1 + number2 + number3) / 3;
+//            Console.WriteLine("El promedio entre los tres numeros ingresados es: " + prom);
+//        }
+//    }
+//}
+//Ejercicio5:
