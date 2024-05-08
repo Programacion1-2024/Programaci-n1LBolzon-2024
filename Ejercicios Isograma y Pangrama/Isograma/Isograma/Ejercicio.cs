eb17bf1c6324066944af052da05c8ca8feffe8a6
Bolzonleo <166229623+Bolzonleo@users.noreply.github.com>
2024-05-08 20:26:04 -0300

    Ejercicio Isograma

--- Ejercicios Isograma y Pangrama/Isograma/Isograma/Ejercicio.cs
+++ Ejercicios Isograma y Pangrama/Isograma/Isograma/Ejercicio.cs
@@ -21,6 +21,7 @@ namespace Isograma
 
             La palabra isogramas, sin embargo, no es un isograma porque la s se repite.
 
+
             TIPS: Funciones ToCharArray, IndexOf y LastIndexOf de la clase String
     */
 
@@ -28,7 +29,16 @@ namespace Isograma
     {
         public static bool IsIsogram(string word)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string PalabraIsograma = word.ToLower().Replace(" ", "").Replace("-", "");    //ToLower para pasar todo el string a minuscula, reemplazo los espacios por nada y los guiones por nada
+
+            for (int i = 0; i < PalabraIsograma.Length; i++) //for para recorrer todos los valores del string 
+            {
+                if (PalabraIsograma.IndexOf(PalabraIsograma[i]) != PalabraIsograma.LastIndexOf(PalabraIsograma[i])) // el indexOf toma el primer valor del string y los compara con los demas para ver cuantos hay, el LastIndexOf toma el ultimo valor y cuenta cuantos tiene, el if verifica que ambos valores sean distintos significa que se repiten
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
